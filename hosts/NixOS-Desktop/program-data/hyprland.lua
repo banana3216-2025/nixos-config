c9ce0c074359c2023bfa93e690f8f2b54990b33a
banana3216-2025 <banana.3216.2025@gmail.com>
2026-05-29 18:04:40 -0500

    changed hyprland keybinds

--- hosts/NixOS-Desktop/program-data/hyprland.lua
+++ hosts/NixOS-Desktop/program-data/hyprland.lua
@@ -282,7 +282,7 @@ hl.bind(mainMod .. " + J",  hl.dsp.focus({ direction = "down" }))
 for i = 1, 10 do
     local key = i % 10 -- 10 maps to key 0
     hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
-    hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
+    hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = tostring(i), follow = false }))
 end
 
 -- Example special workspace (scratchpad)
