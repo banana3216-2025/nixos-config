b74c959a5fde87c73ec6f604faba8e37df028046
banana3216-2025 <banana.3216.2025@gmail.com>
2026-06-16 18:45:11 -0500

    Reordered Flake & Changed kitty color theme to Catppuccin. also added cursor trail

--- flake.nix
+++ flake.nix
@@ -29,6 +29,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    zjstatus = {
+      url = "github:dj95/zjstatus";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
 
     wallpapers.url = "github:banana3216-2025/wallpapers";
@@ -43,40 +48,11 @@
     helium,
     catppuccin,
     quickshell,
+    zjstatus,
     nix-flatpak,
     wallpapers,
     ...
   } @ inputs: {
-    nixosConfigurations.NixOS-Testing = nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit inputs;
-        mainUser = "user";
-      };
-
-      modules = [
-        ./hosts/NixOS-Testing/configuration.nix
-
-        nvf.nixosModules.default
-        home-manager.nixosModules.home-manager
-        ({mainUser, ...}: {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-
-          home-manager.users.${mainUser} = {
-            home.stateVersion = "26.05";
-            home.username = "${mainUser}";
-            home.homeDirectory = "/home/${mainUser}";
-          };
-
-          home-manager.users.root = {
-            home.stateVersion = "26.05";
-            home.username = "root";
-            home.homeDirectory = "/root";
-          };
-        })
-      ];
-    };
-
     nixosConfigurations.NixOS-Desktop = nixpkgs.lib.nixosSystem {
       specialArgs = {
         inherit inputs;
@@ -110,11 +86,16 @@
             gh
           ];
 
+          # Injects the zjstatus flake into the nixpkgs for convenience
+          nixpkgs.overlays = [
+            (final: prev: {
+              zjstatus = inputs.zjstatus.packages.${prev.system}.default;
+            })
+          ];
+
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-
           home-manager.backupFileExtension = "backup";
-
           home-manager.extraSpecialArgs = {inherit inputs;};
 
           home-manager.users.${mainUser} = {
@@ -123,7 +104,6 @@
             home.homeDirectory = "/home/${mainUser}";
 
             xdg.configFile.".gtkrc-2.0".enable = false;
-            # download wallpapers from github
             home.file."Pictures/Wallpapers".source = inputs.wallpapers;
           };
 
@@ -164,9 +144,7 @@
 
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-
           home-manager.backupFileExtension = "backup";
-
           home-manager.extraSpecialArgs = {inherit inputs;};
 
           home-manager.users.${mainUser} = {
@@ -175,8 +153,41 @@
             home.homeDirectory = "/home/${mainUser}";
 
             xdg.configFile.".gtkrc-2.0".enable = false;
-            # download wallpapers from github
             home.file."Pictures/Wallpapers".source = inputs.wallpapers;
+          };
+
+          home-manager.users.root = {
+            home.stateVersion = "26.05";
+            home.username = "root";
+            home.homeDirectory = "/root";
+          };
+        })
+      ];
+    };
+
+    nixosConfigurations.NixOS-Testing = nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs;
+        mainUser = "user";
+      };
+
+      modules = [
+        ./hosts/NixOS-Testing/configuration.nix
+
+        nvf.nixosModules.default
+        home-manager.nixosModules.home-manager
+        ({
+          mainUser,
+          pkgs,
+          ...
+        }: {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.${mainUser} = {
+            home.stateVersion = "26.05";
+            home.username = "${mainUser}";
+            home.homeDirectory = "/home/${mainUser}";
           };
 
           home-manager.users.root = {
