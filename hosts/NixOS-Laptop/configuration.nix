514036e79e1111308fbe08b2aa06458a1cb35004
banana3216-2025 <banana.3216.2025@gmail.com>
2026-08-04 11:02:04 -0500

    Moved hardware config into seperate nix file for NixOS-Laptop

--- hosts/NixOS-Laptop/configuration.nix
+++ hosts/NixOS-Laptop/configuration.nix
@@ -1,5 +1,4 @@
 {
-  config,
   pkgs,
   mainUser,
   inputs,
@@ -28,6 +27,7 @@ in {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./extra-hardware.nix
 
     ../../programs/ghostty.nix
     ../../programs/zsh.nix
@@ -53,81 +53,6 @@ in {
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # Support for nvidia graphics cards
-  boot.kernelParams = [
-    "nvidia-drm.modeset=1"
-    "nvidia-drm.fbdev=1"
-  ];
-
-  boot.initrd.kernelModules = [
-    "nvidia"
-    "nvidia_modeset"
-    "nvidia_uvm"
-    "nvidia_drm"
-  ];
-
-  boot.extraModprobeConfig = ''
-    options nvidia NVreg_RegistryDwords="PowerMizerEnable=0x1; PerfLevelSrc=0x2222; PowerMizerDefaultAC=0x1"
-  '';
-
-  environment.sessionVariables = {
-    GBM_BACKEND = "nvidia-drm";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    NIXOS_OZONE_WL = "1";
-  };
-
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    open = false;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-    powerManagement.finegrained = true;
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-
-      amdgpuBusId = "PCI:5:0:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
-  };
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      vulkan-loader
-      vulkan-validation-layers
-      vulkan-extension-layer
-    ];
-  };
-
-  # enable lower power mode for when using battery only
-  services.auto-cpufreq = {
-    enable = true;
-    settings = {
-      battery = {
-        governor = "powersave";
-        turbo = "never";
-      };
-      charger = {
-        governor = "performance";
-        turbo = "auto";
-      };
-    };
-  };
-
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 8 * 1024;
-    }
-  ];
 
   networking.hostName = "NixOS-Laptop"; # Define your hostname.
 
