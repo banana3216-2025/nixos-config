ea0c09a2fafaab7431059ab0d0d92fc157aaee04
banana3216-2025 <banana.3216.2025@gmail.com>
2026-08-26 22:17:49 -0500

    fixed NAS ip address

--- shared/smb-share.nix
+++ shared/smb-share.nix
@@ -29,7 +29,19 @@ in {
     environment.systemPackages = [pkgs.cifs-utils];
 
     fileSystems."/mnt/share" = {
-      device = "//192.168.2.85/share";
+      device = "//192.168.2.100/share";
+      fsType = "cifs";
+      options = let
+        # Prevent system hangs if the network share becomes unavailable
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+      in [
+        "${automount_opts},credentials=/etc/nixos/smb-secrets"
+        "uid=${toString cfg.userID}"
+        "gid=${toString cfg.groupID}"
+      ];
+    };
+    fileSystems."/mnt/extra" = {
+      device = "//192.168.2.100/extra-drive";
       fsType = "cifs";
       options = let
         # Prevent system hangs if the network share becomes unavailable
